bd0cc5211d207ad4b1385b05076973b388f484ad
Luis Jose De San Jorge Torcate De Pontes <lde-san-@c1r11s7.42porto.com>
2025-04-09 20:38:11 +0100

    42 Progress 03

--- Libft/completed/ft_atoi.c
+++ Libft/completed/ft_atoi.c
@@ -3,70 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lde-san- <lde-san-@student.42porto.com     +#+  +:+       +#+        */
+/*   By: lde-san- <lde-san-@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/18 19:37:26 by lde-san-          #+#    #+#             */
-/*   Updated: 2024/09/18 21:59:07 by lde-san-         ###   ########.fr       */
+/*   Created: 2025/04/09 20:27:18 by lde-san-          #+#    #+#             */
+/*   Updated: 2025/04/09 20:27:23 by lde-san-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-int	ft_atoi(char *str);
-int	real_atoi(int count_signs, char *vessel);
 
-int	ft_atoi(char *str)
-{
-	int		count_signs;
-	int		i;
-	int		guide;
-	char	vessel[11];
-
-	count_signs = 0;
-	guide = 0;
-	i = 0;
-	while (str[i] == ' ')
-	{
-		i++;
-	}
-	while (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			count_signs++;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		vessel[guide++] = str[i];
-		i++;
-	}
-	vessel[guide] = '\0';
-	return (real_atoi(count_signs, &vessel[0]));
-}
-
-int	real_atoi(int count_signs, char *vessel)
-{
-	int	sign;
-	int	result;
-	int	guide;
-
-	sign = 1;
-	guide = 0;
-	result = 0;
-	if ((count_signs % 2) != 0)
-	{
-		sign = -sign;
-	}
-	while (vessel[guide])
-	{
-		result = (result * 10) + (vessel[guide] - '0');
-		guide++;
-	}
-	return (result * sign);
-}
-/*
 #include <stdio.h>
+#include <stdlib.h>
 
-int	main(int arc, char *wordy[])
+int main(int a, char *wordy[])
 {
-	(void)arc;
-	printf("%d\n", ft_atoi(wordy[1]));
-	return (0);
-}*/
+    (void)a;
+    printf("%d\n", atoi(wordy[1]));
+    return 0;
+}
