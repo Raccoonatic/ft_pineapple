a6b9c7a4e677a1c99ae3bc3d9b75554880dfdb2f
Mapache <ljtorcate@gmail.com>
2025-04-10 18:30:36 +0100

    Phone Progress 02

--- Libft/completed/ft_atoi.c
+++ Libft/completed/ft_atoi.c
@@ -10,12 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int a, char *wordy[])
-{
-    (void)a;
-    printf("%d\n", atoi(wordy[1]));
-    return 0;
-}
+int ft_atoi(const char *str)
+{                                                                       int i;                                                          int sign;                                                       int result;                                                                                                                     i = 0;
+        sign = 1;                                                       result = 0;                                                     while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))                  i++;                                                    if(str[i] == '-' || str[i] == '+')                              {                                                                       if(str[i] == '-')                                                       sign *= -1;                                             i++;                                                    }
+        while (str[i] >= '0' && str[i] <= '9')
+	{                                                                       result = result * 10 + (str[i] - '0');                          i++;                                                    }                                                               return(result * sign);                                  }
