399ecd5c08d768630672a11d4ba6d5f35f0511b9
Luis Jose De San Jorge Torcate De Pontes <lde-san-@c1r10s1.42porto.com>
2025-04-14 17:13:19 +0100

    42 Progress 08

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lde-san- <lde-san-@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/14 17:05:10 by lde-san-          #+#    #+#             */
+/*   Updated: 2025/04/14 17:12:42 by lde-san-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ftlib.h"
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
@@ -8,7 +20,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (guide < n)
 	{
 		if (((unsigned char *)s1)[guide] != ((unsigned char *)s2)[guide])
-			return ((int)((((unsigned char *)s1)[guide]) - ((unsigned char *)s2)[guide]))
+			return ((int)((((unsigned char *)s1)[guide]) -
+				((unsigned char *)s2)[guide]));
 		guide++;
 	}
 	return (0);
