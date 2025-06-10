0db54634eed19cec9b37b4c11bbcd077722bc4c1
Mapache <ljtorcate@gmail.com>
2025-06-10 20:00:01 +0100

    Get_Next_line Progress 03

--- Ongoing/get_next_line/test_gnl.c
+++ Ongoing/get_next_line/test_gnl.c
@@ -13,11 +13,11 @@
 #include <stdio.h>
 #include "get_next_line_bonus.h"
 
-#define RSET	"[0m"
-#define B_WI    "[1;37m"
-#define MINT	"[38;2;55;250;133;48;2;0;0;0m"
-#define LIME	"[38;2;0;255;0m"
-#define NEOR	"[3m[38;2;255;153;51m"
+#define RSET	"\033[0m"
+#define B_WI    "\033[1;37m"
+#define MINT	"\033[38;2;55;250;133;48;2;0;0;0m"
+#define LIME	"\033[38;2;0;255;0m"
+#define NEOR	"\033[3m\033[38;2;255;153;51m"
 
 int main(int arc, char *wordy[])
 {
@@ -33,19 +33,11 @@ int main(int arc, char *wordy[])
 
 	if (arc == 1)
 	{
-		printf("
-" B_WI "Wordy options:
-
-" RSET);
-		printf("	" MINT "for stdin_mandatory	" RSET LIME "--> " RSET NEOR "0
-" RSET);
-		printf("	" MINT "for chosenfd_mandatory	" RSET LIME "--> " RSET NEOR "chosenfd
-" RSET);
-		printf("	" MINT "for chosenfd_bonus	" RSET LIME "--> " RSET NEOR "B chosenfd_1 chosenfd_2 chosenfd_... lines_from_each
-
-" RSET);
-		printf(NEOR "note: file names can't start with 'B' or '0'
-" RSET);
+		printf("\n" B_WI "Wordy options:\n\n" RSET);
+		printf("\t" MINT "for stdin_mandatory\t" RSET LIME "--> " RSET NEOR "0\n" RSET);
+		printf("\t" MINT "for chosenfd_mandatory\t" RSET LIME "--> " RSET NEOR "chosenfd\n" RSET);
+		printf("\t" MINT "for chosenfd_bonus\t" RSET LIME "--> " RSET NEOR "B chosenfd_1 chosenfd_2 chosenfd_... lines_from_each\n\n" RSET);
+		printf(NEOR "note: file names can't start with 'B' or '0'\n" RSET);
 		return (1);
 	}
 	
@@ -53,29 +45,19 @@ int main(int arc, char *wordy[])
 	{
 	        if (arc < 4)
 	        {
-	                  printf("
-" B_WI "Wordy options:
-
-" RSET);
-	                  printf("	" MINT "for chosenfd_bonus	" RSET LIME "--> " RSET NEOR "B chosenfd_1 chosenfd_2 chosenfd_... lines_from_each
-
-" RSET);
-		          return(printf("	" MINT "Bonus requires multiple fds, 
-	and last argument must be an 
-	unsigned int. Not" B_WI " '%s'
-" RSET, wordy[arc - 1]), 1);
+	                  printf("\n" B_WI "Wordy options:\n\n" RSET);
+	                  printf("\t" MINT "for chosenfd_bonus\t" RSET LIME "--> " RSET NEOR "B chosenfd_1 chosenfd_2 chosenfd_... lines_from_each\n\n" RSET);
+		          return(printf("\t" MINT "Bonus requires multiple fds, \n\tand last argument must be an \n\tunsigned int. Not" B_WI " ''\n" RSET, wordy[arc - 1]), 1);
 	        }
 	        lines_from_each = atoi(wordy[arc - 1]);
 	        total_fds = arc - 3;
 	        if (lines_from_each < 1)
-	                  return(printf("	" MINT "For Bonus, last argument must be an unsigned int. Not" B_WI " '%s'
-" RSET, wordy[arc - 1]), 1);
+	                  return(printf("\t" MINT "For Bonus, last argument must be an unsigned int. Not" B_WI " ''\n" RSET, wordy[arc - 1]), 1);
 	        
 ////////////////// BONUS ///////////////////			
 			fdeez = malloc(total_fds * sizeof(int));
 			if(!fdeez)
-				return (printf(MINT "Algo no esta piolando papa
-" RSET), 1);
+				return (printf(MINT "Algo no esta piolando papa\n" RSET), 1);
 			guide = 0;
 			while (guide < total_fds)
 			{
@@ -84,8 +66,7 @@ int main(int arc, char *wordy[])
 				else
 					fd = open(wordy[guide + 2], O_RDONLY);
 				if(fd < 0)
-					return (printf(MINT "Algo no esta piolando papa
-" RSET), 1);
+					return (printf(MINT "Algo no esta piolando papa\n" RSET), 1);
 				fdeez[guide] = fd;
 				guide++;
 			}
@@ -98,7 +79,7 @@ int main(int arc, char *wordy[])
 				{
 					while (guide < lines_from_each && ((line = get_next_line(fdeez[fd])) != NULL))
 					{
-						printf("%s", line);
+						printf("", line);
 						free(line);
 						guide++;
 					}
@@ -132,14 +113,13 @@ int main(int arc, char *wordy[])
 	
 	if (fd < 0)
 	{
-		printf(MINT "Algo no esta piolando papa
-" RSET);
+		printf(MINT "Algo no esta piolando papa\n" RSET);
 		printf(NEOR "note: file names can't start with 'B' or '0'" RSET);
 		return (1);
 	}
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		printf("%s", line);
+		printf("", line);
 		free(line);
 	}
 	close(fd);
@@ -169,9 +149,9 @@ int main(int arc, char *wordy[])
 					continue ;
 				}
 				if (wordy[1][1] == 'a')
-				        printf(MINT "%s" RSET, line);
+				        printf(MINT "" RSET, line);
 				else
-				        printf(B_WI "%s" RSET, line);
+				        printf(B_WI "" RSET, line);
 				free(line);
 			}
 			close(fd);
@@ -217,7 +197,7 @@ int main(int arc, char *wordy[])
 	                                else if (line[guide] == ' ')
 		                                write(1, "@", 1);
 	                                else
-		                                write(1, &line[guide], 1);
+		                                write(1, &line[guide], 1); // Keep unchanged chars
 	                                guide++;
                                 }
 				free(line);
