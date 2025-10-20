1458f2062a279d085546fccd68e83bf2c7d1d49f
Mapache <ljtorcate@gmail.com>
2025-10-20 21:36:13 +0100

    Pipex update 08

--- Ongoing/Pipex/pipex.h
+++ Ongoing/Pipex/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lde-san- <lde-san-@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 23:10:50 by lde-san-          #+#    #+#             */
-/*   Updated: 2025/10/19 22:19:46 by lde-san-         ###   ########.fr       */
+/*   Updated: 2025/10/20 19:48:23 by lde-san-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,12 @@
 
 # define RSET	"\033[0m" 
 # define B_WI	"\033[1;37m"
-# define BLAK	"\033[38;2;0;0;0m"
 # define LIME	"\033[38;2;0;255;0m"
 # define BABY	"\033[38;2;0;255;247m"
 # define BLOD	"\033[1;38;2;255;0;0m"
 # define PURP	"\033[1;38;2;174;5;252m"
+# define MINT	"\033[1;38;2;55;250;133m"
 # define NEOR	"\033[3m\033[38;2;255;153;51m"
-# define MINT	"\033[1;38;2;55;250;133;48;2;0;0;0m"
 
 char	*px_get_pathname(char *filename, char *envp[]);
 void	px_free_matrix(char **matrix);
